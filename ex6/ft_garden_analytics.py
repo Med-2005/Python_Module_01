7b60669d369a1566b6b2c13dbe0645503cc3305f
mryahi-e <ryahisimo331@gmail.com>
2026-01-05 18:04:40 +0100

    push

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -10,11 +10,28 @@ class FloweringPlant(Plant):
         super().__init__(name, height)
         self.color = color
 
-    def bloom(self):
-        pass
-
 
 class PrizeFlower(FloweringPlant):
     def __init__(self, name, height, color, points):
         super().__init__(name, height, color)
         self.points = points
+
+
+class GardenManager:
+
+    total_gardens = 0
+
+    def __init__(self, owner_name):
+        self.owner_name = owner_name
+        self.plants = []
+        GardenManager.total_gardens += 1
+
+    def add_plants(self, plant: Plant):
+        self.plants.append(plant)
+        print(f"Added {plant.name} to {self.owner_name}'s garden")
+
+
+my_garden = GardenManager("Mohamed")
+rose = Plant("Rose", 25)
+my_garden.add_plants(rose)
+print(GardenManager.total_gardens)
